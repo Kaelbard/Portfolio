ec92a9ac5e40aeec5b846bf59a583381195d61a8
Christiano Barbosa <kaelbard@gmail.com>
2023-06-27 14:15:45 -0300

    atualização

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,47 +4,22 @@ import { projects } from "../data";
 
 export default function Projects() {
   return (
-    <section id="projects" className="text-gray-400 bg-gray-900 body-font">
-      <div className="container px-5 py-5 mx-auto text-center lg:px-40">
+    <section id="projects" className="bg-cover bg-center" style={{ backgroundImage: `url('./quem_somos.png')` }}>
+      <div className="container px-6 pt-20 mx-auto text-center lg:px-40 justify-center ">
         <div className="flex flex-col w-full mb-20 items-center">
-          <CodeIcon className="mx-auto inline-block w-10 mb-4" />
-          <div className="items-center justify-center">
-            <div className="container w-80 bg-pink-500 py-1 flex justify-center items-center">
-              <h1 className="sm:text-xl font-medium title-font mb-0 text-white">
-                Quem somos nós
-              </h1>
-            </div>
-            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
-              facilis repellat ab cupiditate alias vero aliquid obcaecati quisquam
-              fuga dolore.
-            </p>
+          <div className=" px-14 py-3 justify-center bg-pink-500 items-center ">
+            <h1 className="sm:text-4xl font-medium title-font mb-0 text-white">
+              Quem somos nós
+            </h1>
           </div>
-        </div>
-        <div className="flex flex-wrap -m-4">
-          {projects.map((project) => (
-            <a
-              href={project.link}
-              key={project.image}
-              className="sm:w-1/2 w-100 p-4">
-              <div className="flex relative">
-                <img
-                  alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
-                  src={project.image}
-                />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
-                    {project.subtitle}
-                  </h2>
-                  <h1 className="title-font text-lg font-medium text-white mb-3">
-                    {project.title}
-                  </h1>
-                  <p className="leading-relaxed">{project.description}</p>
-                </div>
-              </div>
-            </a>
-          ))}
+          <div className="column-2">
+            <div className="">
+              <p>Olá</p>
+            </div>
+            <div className="">
+              <p>Olá</p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
--- src/components/What.js
+++ src/components/What.js
@@ -3,13 +3,13 @@ import React from "react";
 export default function What() {
     return (
         <section id="what" className= "flex-grow bg-white bg-cover bg-center" style={{ backgroundImage: `url('./background-what.png')` }}>
-            <div className="justify-center flex pl-48 py-72 md:flex-row flex-col items-center">
-                <div className="lg:flex md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-black">
+            <div className="container flex pl-72 py-40 md:flex-row flex-col items-center">
+                <div className="lg:flex md:w-1/2 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+                    <h1 className="title-font sm:text-5xl mb-3 font-medium text-black text-left">
                     Se você ainda não investe em UX/UI, você está perdendo clientes!
                         <br className="hidden lg:inline-block" />
                     </h1>
-                    <p className="mb-20 leading-relaxed text-black">
+                    <p className="mb-20 leading-relaxed sm:text-lg text-black text-justify">
                     75% dos usuários julgam a credibiliade de uma empresa com base em seu design de site, de acordo com a pesquisa da Stanford University. Um estudo da Forrester concluiu que cada dólar investido em UX pode resultar em um retorno de investimento (ROI) de até 100 dólares. De acordo com a Nielsen Norman Group, a má experiência do usuário pode levar até 88% dos usuários a deixar um site ou aplicativo.</p>
                 </div>
                 <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6"></div>
